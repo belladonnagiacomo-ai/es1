d5e10d0ce7942de06c7ad1d7a4a4b840c8931d00
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-01-28 13:33:47 +0100

    mod

---
+++ es3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace es3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dimmi il tuo nome: ");
+            string nome = Console.ReadLine();
+            char[] n = nome.ToCharArray();
+            Console.WriteLine("Dimmi il tuo cognome: ");
+            string cognome = Console.ReadLine();
+            char[] c = cognome.ToCharArray();
+            Console.WriteLine(nome[0] + " " + cognome[0]);
+        }
+    }
+}
