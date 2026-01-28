e516fe2f2cefa092c21a1b227b81e971a7987271
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-01-28 13:26:32 +0100

    mod

---
+++ es2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace es2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "giacomo@gmail.com";
+            string[] n = nome.Split("@");
+            Console.WriteLine(n[1]);
+        }
+    }
+}
