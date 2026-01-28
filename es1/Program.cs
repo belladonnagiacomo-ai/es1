96454cd434d079d9303f5d9f94d5392657e753bc
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-01-28 13:20:23 +0100

    Aggiungere i file di progetto.

---
+++ es1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace es1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci il tuo nome completo ");
+            string nome = Console.ReadLine();
+            string n = nome.Trim();
+            Console.WriteLine(n.ToUpper());
+            string p = n.ToUpper();
+            int l = n.Length;
+            Console.WriteLine(l);
+        }
+    }
+}
