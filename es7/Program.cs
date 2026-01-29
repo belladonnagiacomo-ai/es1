c1aab73d41a36022fabdf7340960a0fe59180a86
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-01-29 10:01:08 +0100

    mod

---
+++ es7/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace es7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dammi una frase: ");
+            string frase = Console.ReadLine();
+            char[] parole = frase.ToCharArray();
+            char[] lettera = {'a','A'};
+            int z = 0;
+            for(int i = 0; i < parole.Length; i++)
+            {
+                if (lettera.Contains(parole[i]))
+                {
+                    z++;
+                }
+            }
+            if(z > 0)
+            {
+                Console.WriteLine("la lettera a e contenuta nella frase " + frase + " per " + z);
+            }
+        }
+    }
+}
