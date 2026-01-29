d1c35cb64ffb72f3b094b1b1fd902a017d1582a6
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-01-29 10:24:32 +0100

    mod

---
+++ es9/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace es9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dammi una data: ");
+            string data = Console.ReadLine();
+            string[] Data = data.Split('/');
+            string data2;
+            for (int i = Data.Length - 1; i >= 0; i--)
+            {
+               
+                Console.Write(Data[i]);
+                if (i > 0)
+                {
+                    Console.Write("/");
+                }
+            }
+           
+            
+        }
+    }
+}
