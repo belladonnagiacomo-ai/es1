6f08e6db8b0b7b87d70909eabb7349d8e09f83ec
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-01-29 10:37:49 +0100

    mpd

---
+++ es10/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace es10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dammi una parola");
+            string parola = Console.ReadLine();
+            char[] ParolaT = parola.ToCharArray();
+            char[] p = parola.ToCharArray();
+            for (int i = ParolaT.Length - 1; i >= 0; i--)
+            {
+                Console.Write(ParolaT[i]);
+                if (p == ParolaT)
+                {
+                    Console.WriteLine("le parole sono palindromi");
+                }
+            }
+          
+           
+
+            
+        }
+    }
+}
