d57b3e61884600c93918f36e2062787572d1ccde
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-01-29 09:22:27 +0100

    mod

---
+++ es5/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace es5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string voti = ("8,7,5,4,9");
+            string[] voto = voti.Split(",");
+            for(int i = 0; i < voto.Length; i++)
+            {
+                Console.WriteLine(voto[i]);
+            }
+        }
+    }
+}
