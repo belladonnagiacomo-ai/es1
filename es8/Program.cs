56671dd33786c6852e08268c6b9aefaf3e17a211
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-01-29 10:09:38 +0100

    mod

---
+++ es8/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace es8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dammi una parola");
+            string parola = Console.ReadLine();
+            char[] Parola = parola.ToCharArray();
+            for (int i = Parola.Length - 1; i>= 0; i--)
+            {
+                Console.Write(Parola[i]);
+            }
+        }
+    }
+}
