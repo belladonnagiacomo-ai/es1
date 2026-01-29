c9205906fe1e096983e816619deb54e3541a3518
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-01-29 09:44:58 +0100

    mod

---
+++ es6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace es6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dammi un file(compresa l'esetnsione)");
+            string file = Console.ReadLine();
+            bool estensione = file.EndsWith(".pdf");
+            if(estensione == true)
+            {
+                Console.WriteLine(estensione);
+            }
+            else
+            {
+                Console.WriteLine(estensione);
+            }
+        }
+    }
+}
