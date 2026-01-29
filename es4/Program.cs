e6c57ce7eb259334f0d259165796bcb6d99e826b
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-01-29 09:14:18 +0100

    mod

---
+++ es4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace es4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dimmi una frase: ");
+            string frase = Console.ReadLine();
+            string nuova = frase.Replace("brutto", "***");
+            Console.WriteLine(nuova);
+        }
+    }
+}
